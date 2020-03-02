f5a89c07b7127aa11f322fc5641a35f169d95df4
ffardo <ffardo@geru.com.br>
2020-03-02 11:36:46 -0300

    remove post_load

--- keyloop/schemas/auth_session.py
+++ keyloop/schemas/auth_session.py
@@ -1,9 +1,5 @@
-import arrow
 import marshmallow_jsonapi
 import marshmallow
-
-from keyloop.interfaces.identity import IIdentity, IIdentitySource
-from keyloop.interfaces.auth_session import IAuthSession, IAuthSessionSource
 
 
 class AuthSessionSchema(marshmallow_jsonapi.Schema):
@@ -24,42 +20,3 @@ class AuthSessionSchema(marshmallow_jsonapi.Schema):
         schema="IdentitySchema",
         dump_only=True,
     )
-
-    @marshmallow.pre_load
-    def load_identity(self, data, **kwargs):
-        # import pdb
-        # pdb.set_trace()
-        # new_data = self.format_json_api_response(data, False)
-        data['bla'] = "bla"
-        return data
-
-    @marshmallow.post_load
-    def validate_credentials(self, data, **kwargs):
-
-        request = self.context.request
-        registry = request.registry.settings["keyloop_adapters"]
-
-        identity_provider = registry.lookup(
-            [IIdentity], IIdentitySource, request.context.realm
-        )
-        if not identity_provider:
-            # need to find a way to return a 404 instead of a 400
-            raise marshmallow.ValidationError("Realm failed")
-
-        identity = identity_provider.get(data["username"])
-
-        if not identity.login(data["username"], data["password"]):
-            # need to find a way to return a 401 instead of a 400
-            raise marshmallow.ValidationError("credentials failed")
-
-        session_provider = registry.lookup(
-            [IAuthSession], IAuthSessionSource, request.context.realm
-        )
-        auth_session = session_provider.create(identity=identity,
-                                               ttl=600,
-                                               active=True,
-                                               start=arrow.utcnow().datetime)
-
-        # Fixme
-        request.identity = identity
-        request.auth_session = auth_session
