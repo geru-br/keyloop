1ffa21dbf00bb8b66348af3fc6bcad9182885de8
Guilherme Pereira <guilherme@geru.com.br>
2020-03-13 15:47:49 -0300

    Fix/Add permission_grant tests

--- tests/api/v1/test_permission_grant.py
+++ tests/api/v1/test_permission_grant.py
@@ -20,12 +20,13 @@ def perm_grant_payload():
 
 class TestGrantPermission:
 
-    def test_grant_permission_success(self, pyramid_app, perm_grant_payload, user, permission, fake_perm_grant_class):
+    def test_grant_permission_success(self, pyramid_app, perm_grant_payload, user, permission):
         res = pyramid_app.post_json(f"/api/v1/realms/REALM/identities/{user.id}/permissions",
                                     perm_grant_payload,
                                     content_type="application/vnd.api+json",
                                     status=200)
 
+        assert (str(permission.uuid), str(user.id)) in user.PERMISSION_GRANTS
         assert res.content_type == "application/vnd.api+json"
         assert res.json == {
             "data": {
@@ -39,7 +40,8 @@ class TestGrantPermission:
         }
 
     def test_identity_not_found(self, pyramid_app, perm_grant_payload, user):
-        res = pyramid_app.post_json("/api/v1/realms/REALM/identities/9f533cff-a6b3-4430-b1b9-557da85e0121/permissions",
+        inexistent_identity_id = "9f533cff-a6b3-4430-b1b9-557da85e0121"
+        res = pyramid_app.post_json(f"/api/v1/realms/REALM/identities/{inexistent_identity_id}/permissions",
                                     perm_grant_payload,
                                     content_type="application/vnd.api+json",
                                     status=404)
@@ -52,6 +54,49 @@ class TestGrantPermission:
                     "location": "path",
                     "name": "identity_id",
                     "description": "Identity not found"
+                }
+            ]
+        }
+
+    def test_permission_not_found(self, pyramid_app, perm_grant_payload, user):
+        perm_grant_payload["data"]["attributes"]["perm_name"] = "crazy-permission"
+        res = pyramid_app.post_json(f"/api/v1/realms/REALM/identities/{user.id}/permissions",
+                                    perm_grant_payload,
+                                    content_type="application/vnd.api+json",
+                                    status=400)
+
+        assert res.content_type == "application/vnd.api+json"
+        assert res.json == {
+            "status": "error",
+            "errors": [
+                {
+                    "location": "body",
+                    "name": "perm_name",
+                    "description": "Permission not found"
+                }
+            ]
+        }
+
+    def test_permission_already_granted_to_identity(self, pyramid_app, perm_grant_payload, user, permission):
+        pyramid_app.post_json(f"/api/v1/realms/REALM/identities/{user.id}/permissions",
+                              perm_grant_payload,
+                              content_type="application/vnd.api+json",
+                              status=200)
+
+        assert {(str(permission.uuid), str(user.id))} == user.PERMISSION_GRANTS
+
+        res = pyramid_app.post_json(f"/api/v1/realms/REALM/identities/{user.id}/permissions",
+                                    perm_grant_payload,
+                                    content_type="application/vnd.api+json",
+                                    status=400)
+        assert res.content_type == "application/vnd.api+json"
+        assert res.json == {
+            "status": "error",
+            "errors": [
+                {
+                    "location": "body",
+                    "name": "perm_name",
+                    "description": "Permission already granted to identity"
                 }
             ]
         }
